95f9a70ccdf106062eedfbe4c92890bfe15f050e
seattlenomnom <mark.crapser@gmail.com>
2023-08-11 22:16:28 -0700

    created doubly linked list

--- ptr11p5.c
+++ ptr11p5.c
@@ -7,23 +7,62 @@
 
 /* includes */
 
-#incluce <stdio.h>
+#include <stdio.h>
 
 /* defines */
 
 
+
 /* data definitions */
+
+struct entry{
+    int value;
+    struct entry *next;
+    struct entry *prev;
+};
+
 
 
 /* function declarations */
 
-
+void printList(struct entry *p2list);
 
 
 /* main */
 
 int main(int argc, char *argv[]) {
 
+/* create list components */
+
+    struct entry *p2head, n1, n2, n3, n4, n5;
+
+
+/* init values */
+
+    p2head = &n1;
+    n1.value = 100;
+    n2.value = 200;
+    n3.value = 300;
+    n4.value = 400;
+    n5.value = 500;
+
+/* hook entries together to form a doubly linked list */
+
+
+    n1.next = &n2;
+    n1.prev = &n5;
+
+    n2.next = &n3;
+    n2.prev = &n1;
+
+    n3.next = &n4;
+    n3.prev = &n2;
+
+    n4.next = &n5;
+    n4.prev = &n3;
+
+    n5.next = &n1;
+    n5.prev = &n4;
 
 
 
@@ -39,3 +78,18 @@ int main(int argc, char *argv[]) {
 }
 
 /* function defiinitions */
+
+void printList(struct entry *p2list){
+
+
+
+
+
+
+
+
+
+
+
+}
+
